9d06dc0a23596c2cfdcd6911f747e99a8d4363ba
boddupillivasu <boddupillibujji123@gmail.com>
2024-06-24 13:43:22 +0530

    first commit

---
+++ Primitives.java
@@ -0,0 +1,43 @@
+package com.primitives.java;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        //workers information
+
+        String name;
+        name = "rajesh";
+        System.out.println("the worker name: " + name);
+
+        byte id;
+        id = 10;
+        System.out.println("the worker id :" + id);
+
+        short salary;
+        salary = 25000;
+        System.out.println("the worker salary:" + salary);
+
+        long number;
+        number = 1234535l;
+        System.out.println("the worker location: " + number);
+
+        char symbol;
+        symbol = 'R';
+        System.out.println("the worker symbol:" + symbol);
+
+        float rank;
+        rank = 12.0f;
+        System.out.println("the worker rank:" + rank);
+
+        double rankPosition;
+        rankPosition = 123.34;
+        System.out.println("the worker rankPosition:" + rankPosition);
+
+        boolean information;
+        information = true;
+        System.out.println("the worker information:" + information);
+
+
+    }
+}
+
